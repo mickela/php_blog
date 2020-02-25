81b58b39039cc991db10bfbfae70aff5275c6262
Michael <osoba.michael@outlook.com>
2020-02-26 00:11:15 +0100

    Created Post model

---
+++ models/Post.php
@@ -0,0 +1,47 @@
+<?php
+  class Post{
+    // params
+    private $conn;
+    private $table = 'posts';
+
+    // post properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
+
+    // constructor
+    public function __construct($db){
+      $this->conn = $db;
+    }
+
+    // Get posts
+    public function read(){
+      // create query
+      $query = 'SELECT
+            c.name as category_name,
+            p.id,
+            p.category_id,
+            p.title,
+            p.body,
+            p.author,
+            p.created_at
+            FROM
+            ' . $this->table . ' p
+            LEFT JOIN
+              categories c ON p.category_id = c.id
+            ORDER BY
+              p.created_at DESC';
+      // prepared PDOStatement
+      $stmt = $this->conn->prepare($query);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
+    }
+
+  }
